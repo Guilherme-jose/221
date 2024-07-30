26bb20281ad753c105d5582150c4eb0458e4ca3f
Guilherme <guilherme.j.lopes@yfv.br>
2024-07-30 15:06:07 -0300

    for i in range(100):     for j in range(100):         graph[(i, j)] = []         if i != 0:             graph[(i, j)].append((i - 1, j))             if j != 0:                 graph[(i, j)].append((i - 1, j - 1))             if j != 99:                 graph[(i, j)].append((i - 1, j + 1))         if i != 99:             graph[(i, j)].append((i + 1, j))             if j != 0:                 graph[(i, j)].append((i + 1, j - 1))             if j != 99:                 graph[(i, j)].append((i + 1, j + 1))         if j != 0:             graph[(i, j)].append((i, j - 1))         if j != 99:             graph[(i, j)].append((i, j + 1))
    start = (0, 0)
    end = (99, 99)

--- 221.py
+++ 221.py
@@ -1,32 +1,39 @@
 
 from a_star import a_star
 
-graph = {}
 
 
-for i in range(100):
-    for j in range(100):
-        graph[(i, j)] = []
-        if i != 0:
-            graph[(i, j)].append((i - 1, j))
-            if j != 0:
-                graph[(i, j)].append((i - 1, j - 1))
-            if j != 99:
-                graph[(i, j)].append((i - 1, j + 1))
-        if i != 99:
-            graph[(i, j)].append((i + 1, j))
-            if j != 0:
-                graph[(i, j)].append((i + 1, j - 1))
-            if j != 99: 
-                graph[(i, j)].append((i + 1, j + 1))
-        if j != 0:
-            graph[(i, j)].append((i, j - 1))
-        if j != 99:
-            graph[(i, j)].append((i, j + 1))
+class pathfinder:
+    graph = {}
+    
+    def create_graph(self):
+        for i in range(100):
+            for j in range(100):
+                self.graph[(i, j)] = []
+                if i != 0:
+                    self.graph[(i, j)].append((i - 1, j))
+                    if j != 0:
+                        self.graph[(i, j)].append((i - 1, j - 1))
+                    if j != 99:
+                        self.graph[(i, j)].append((i - 1, j + 1))
+                if i != 99:
+                    self.graph[(i, j)].append((i + 1, j))
+                    if j != 0:
+                        self.graph[(i, j)].append((i + 1, j - 1))
+                    if j != 99: 
+                        self.graph[(i, j)].append((i + 1, j + 1))
+                if j != 0:
+                    self.graph[(i, j)].append((i, j - 1))
+                if j != 99:
+                    self.graph[(i, j)].append((i, j + 1))
+            
         
-start = (0, 0)
-end = (99, 99)
+    def solve(self, algorithm, start, end):
+        algorithm(self.graph, start, end)
+        
+        
+pathfinder = pathfinder()
 
+pathfinder.create_graph()
 
-
-print(a_star(graph, start, end))
+pathfinder.solve(a_star, (0, 0), (99, 99))
